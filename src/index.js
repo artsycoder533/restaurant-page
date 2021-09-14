bd18b44f9f664852530fddb627b830dbdf27fac8
Natasha <njohnson533@gmail.com>
2021-09-14 01:57:45 -0400

    added style.css, added css loader and style loader to webpack.config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("This is so cool!!!");
+import "./style.css";
+import hero from "./taco.png";
+
+const heroImg = new Image();
+heroImg.classList.add("img");
+heroImg.src = hero;
+const container = document.getElementById("content");
+container.appendChild(heroImg);
