1bb61c49f947de1acc75b07bff0d9c2ad099a807
Natasha <njohnson533@gmail.com>
2021-09-15 02:32:39 -0400

    changed mode to development, adjusted spacing in style.css

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	mode: "production",
+	mode: "deployment",
 	entry: "./src/index.js",
 	output: {
 		filename: "bundle.js",
